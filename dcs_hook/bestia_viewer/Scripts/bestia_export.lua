9f3e1719a78769abbb394b69573e03cddb09f9e7
Huambo <you@example.com>
2023-06-16 21:02:23 +0200

    exporting works

--- dcs_hook/bestia_viewer/Scripts/bestia_export.lua
+++ dcs_hook/bestia_viewer/Scripts/bestia_export.lua
@@ -2,44 +2,60 @@ log.write('Bestia', log.INFO, 'Bestia Starte')
 
 Bestia = {}
 
-function Bestia.table_serialize(tab, max_depth)
+function Bestia.left_pad(n)
+	local result = ""
+	for i=1, n do
+		result = result .. "\t"
+	end
+	return result
+end
+
+function Bestia.table_serialize(tab, max_depth, indent)
+	indent = indent or 0
 	max_depth = max_depth or 10
 	if max_depth == 0 then
 		return "MAX_DEPTH"
 	end
 
     if type(tab) == "table" then
-		local result = "{"
+		local result = "\n" .. Bestia.left_pad(indent) .. "{\n"
         for k, v in pairs(tab) do
-            result =  result .. k .. " = " .. Bestia.table_serialize(v, max_depth - 1) .. ","
+            result =  result .. Bestia.left_pad(indent + 1) .. k .. " = " .. Bestia.table_serialize(v, max_depth - 1, indent + 1) .. ",\n"
         end
-        return result .. "}"
+        return result .. Bestia.left_pad(indent) .. "}"
     elseif type(tab) == "number" then
         return tostring(tab)
     elseif type(tab) == "string" then
-        return tab
+        return "\"" .. tab .. "\""
     elseif type(tab) == "boolean" then
-        return tab and "true" or "false"
+        return (tab and "true" or "false")
     else
         return "UNKNOWN_TYPE: " .. type(tab)
     end
 end
 
-function Bestia.LuaExportStart()
+function LuaExportStart()
 	log.write('Bestia', log.INFO, 'Export Start')
 end
 
-function Bestia.LuaExportBeforeNextFrame()
-	log.write('Bestia', log.INFO, "Every unit: " .. Bestia.table_serialize(LoGetWorldObjects("units")))
+function LuaExportBeforeNextFrame()
+
+	
 end
 
-function Bestia.LuaExportAfterNextFrame()
-
+function LuaExportAfterNextFrame()
+	local status, err = pcall(
+		function () 
+			log.write('Bestia', log.INFO, "Kinda works")
+			log.write('Bestia', log.INFO, "Every unit: " .. Bestia.table_serialize(LoGetWorldObjects("units"), 5))
+		end
+	)
+	log.write('Bestia', log.ERROR, err)
 end
 
-function Bestia.LuaExportStop ()
+function LuaExportStop ()
 	log.write('Bestia', log.INFO, 'Export Stop')
 end
 
 
-log.write('Bestia', log.ERROR, 'Bestia Ende')
+log.write('Bestia', log.INFO, 'Bestia Ende')
