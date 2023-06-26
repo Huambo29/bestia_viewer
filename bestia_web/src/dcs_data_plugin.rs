a497a63a1d19aedea8cf732ccf13dae0fe2b1ac6
Huambo <bitru29@gmail.com>
2023-06-26 10:06:41 +0200

    requests work

--- bestia_web/src/dcs_data_plugin.rs
+++ bestia_web/src/dcs_data_plugin.rs
@@ -1,26 +1,53 @@
 use bevy::prelude::*;
+use crossbeam_channel::{unbounded, Receiver};
 
 pub struct DCSDataPlugin;
 
 impl Plugin for DCSDataPlugin {
 	fn build(&self, app: &mut App){
 		info!("lul");
-		app.add_startup_system(dcs_data_update_system);
+		app.add_startup_system(dcs_data_update_system)
+			.add_system(read_stream);
 	}
 }
 
-fn get_dcs_data() {
-	wasm_bindgen_futures::spawn_local(async move {
+//fn process_dcs_csv(dcs_data: &str) {
+//
+//}
+
+#[derive(Resource, Deref)]
+struct DCSDataReceiver(Receiver<String>);
+
+fn dcs_data_update_system(mut commands: Commands, time: Res<Time>) {
+	let (dcs_data_tx, dcs_data_rx) = unbounded::<String>();
+
+	wasm_bindgen_futures::spawn_local(async move{
 		let client = reqwest::Client::new();
-		//let txt = reqwest::get("http://127.0.0.1:2137/ping").await;
-		let txt = client
-        	.get("http://127.0.0.1:2137/ping")
-        	.send()
-        	.await;
-		info!("test: {:?}", txt);
+			
+		let mut i = 1;
+		loop {
+			i += 1;
+			if i >= 200 {
+				break;
+			}
+			let txt_content = client
+        		.get("http://127.0.0.1:2137/units")
+        		.send()
+        		.await
+				.unwrap()
+				.text()
+				.await
+				.unwrap();
+			info!("request: {} units:\n{}", i, txt_content);
+			//dcs_data_tx.send(txt).unwrap();
+		}
 	});
+
+	commands.insert_resource(DCSDataReceiver(dcs_data_rx));
 }
 
-fn dcs_data_update_system() {
-	get_dcs_data();
+fn read_stream(receiver: Res<DCSDataReceiver>) {
+	for from_stream in receiver.try_iter() {
+        info!("test stream: {}", from_stream);
+    }
 }
